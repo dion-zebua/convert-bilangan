edf86b889206dd71ac455ed66e8eba37cedf67bc
Kurniawann12 <kurniawanahkam@gmail.com>
2024-05-10 20:59:40 +0700

    aku cinta simdig

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./script.js"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      keyframes: {
+        rotate: {
+          '0% , 100%': {
+            '--tw-gradient-via-position': '30%',
+          },
+          '10%': {
+            '--tw-gradient-via-position': '40%',
+          },
+          '20%': {
+            '--tw-gradient-via-position': '50%',
+          },
+          '30%': {
+            '--tw-gradient-via-position': '60%',
+          },
+          '40%': {
+            '--tw-gradient-via-position': '70%',
+          },
+          '50%': {
+            '--tw-gradient-via-position': '80%',
+          },
+          '60%': {
+            '--tw-gradient-via-position': '70%',
+          },
+          '70%': {
+            '--tw-gradient-via-position': '60%',
+          },
+          '80%': {
+            '--tw-gradient-via-position': '50%',
+          },
+          '90%': {
+            '--tw-gradient-via-position': '40%',
+          },
+        }
+      }
+    }
+  },
+  plugins: [],
+}
+
